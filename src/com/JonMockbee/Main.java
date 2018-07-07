8fb72e5b1133e241d277e3176920a7757187c349
jmockbee <mockbee1000@gmail.com>
2018-07-07 14:21:39 -0400

    initial commit

---
+++ src/com/JonMockbee/Main.java
@@ -0,0 +1,16 @@
+package com.JonMockbee;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // VCS at top  enable version control integration (means git init)
+        // choose git and then ok at bottom bring up version control bottom left  hand corner
+        // will see files in unversioned files
+        // under project and intellejdemo create file for git.ignore
+        //create .gitignore file put .idea and *iml. file in it
+        // ctrl+a means select all
+
+    }
+}
